5cc7185573f4cbc29527ebb6e18fa5872f4a744d
Yannick Mahe <mahe.yannick@gmail.com>
2012-11-27 22:13:33 +0100

    error in theme

--- backend/themes/bootstrap/layout.html.php
+++ backend/themes/bootstrap/layout.html.php
@@ -6,6 +6,6 @@
 <body>
 	<?php include_theme_partial('header'); ?>
 	<?php echo $page_content; ?>
-	<?php include_theme_partial('header'); ?>
+	<?php include_theme_partial('footer'); ?>
 	<?php include_theme_js(); ?>
 </body>
