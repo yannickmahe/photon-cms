4473c8d427508a44ec7795d6568e3f632f8748d5
Yannick Mahe <mahe.yannick@gmail.com>
2012-11-27 22:31:43 +0100

    change default values

--- backend/db/init.sql
+++ backend/db/init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE page (id INTEGER PRIMARY KEY, title STRING, url STRING, body_html TEXT);
 INSERT INTO page (title, url, body_html) VALUES ('Home','/','Homepage');
-INSERT INTO page (title, url, body_html) VALUES ('404','/404.html','404 Not found');
-INSERT INTO page (title, url, body_html) VALUES ('500','/500.html','500 Not found');
+INSERT INTO page (title, url, body_html) VALUES ('404','/404.html','<h1>404 Not found</h1>');
+INSERT INTO page (title, url, body_html) VALUES ('500','/500.html','<h1>500 Not found</h1>');
 
 CREATE TABLE layout_item (id INTEGER PRIMARY KEY, name STRING, html TEXT);
 
