2fd4551f0932b55725d7d3b5a3553695cdc18b19
Yannick Mahe <mahe.yannick@gmail.com>
2012-11-27 22:33:41 +0100

    formatting init

--- backend/db/init.sql
+++ backend/db/init.sql
@@ -1,9 +1,9 @@
 CREATE TABLE page (id INTEGER PRIMARY KEY, title STRING, url STRING, body_html TEXT);
-INSERT INTO page (title, url, body_html) VALUES ('Home','/','Homepage');
-INSERT INTO page (title, url, body_html) VALUES ('404','/404.html','<h1>404 Not found</h1>');
-INSERT INTO page (title, url, body_html) VALUES ('500','/500.html','<h1>500 Not found</h1>');
+	INSERT INTO page (title, url, body_html) VALUES ('Home','/','Homepage');
+	INSERT INTO page (title, url, body_html) VALUES ('404','/404.html','<h1>404 Not found</h1>');
+	INSERT INTO page (title, url, body_html) VALUES ('500','/500.html','<h1>500 Not found</h1>');
 
 CREATE TABLE layout_item (id INTEGER PRIMARY KEY, name STRING, html TEXT);
 
 CREATE TABLE config (id INTEGER PRIMARY KEY, name STRING, html STRING);
-INSERT INTO config (name, value) VALUES ('theme','bootstrap');
+	INSERT INTO config (name, value) VALUES ('theme','bootstrap');
